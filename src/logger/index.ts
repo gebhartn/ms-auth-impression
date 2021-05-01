c83e43be9473cd5161ad0cde87919c56326c69b6
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-05-01 16:16:47 -0700

    ci: pushing for test coverage

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,20 +1,24 @@
 import winston from 'winston';
 
-const Logger = winston.createLogger({
-    level: process.env.NODE_ENV !== 'production' ? 'debug' : 'warn',
-    levels: {
-        error: 0,
-        warn: 1,
-        info: 2,
-        http: 3,
-        debug: 4,
-    },
-    format: winston.format.combine(
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
-        winston.format.colorize({ all: process.env.NODE_ENV !== 'production' }),
-        winston.format.printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`),
-    ),
-    transports: [new winston.transports.Console()],
-});
+export const makeLogger = () => {
+    return winston.createLogger({
+        level: process.env.NODE_ENV === 'production' ? 'warn' : 'debug',
+        levels: {
+            error: 0,
+            warn: 1,
+            info: 2,
+            http: 3,
+            debug: 4,
+        },
+        format: winston.format.combine(
+            winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+            winston.format.colorize({ all: process.env.NODE_ENV !== 'production' }),
+            winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`),
+        ),
+        transports: [new winston.transports.Console()],
+    });
+};
+
+const Logger = makeLogger();
 
 export default Logger;
--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,5 +1,27 @@
-it('can run the index.ts file', async () => {
-    const module = await import('../../src/index');
+import { makeLogger } from '../../src/logger';
 
-    expect(module).toBeDefined();
+describe('index', () => {
+    it('can run the index.ts file', async () => {
+        const module = await import('../../src/index');
+
+        expect(module).toBeDefined();
+    });
+});
+
+describe('logger', () => {
+    it('logs the correct level in production', async () => {
+        process.env.NODE_ENV = 'production';
+
+        const logger = makeLogger();
+
+        expect(logger.level).toBe('warn');
+    });
+
+    it('logs the correct level in non-prod', async () => {
+        process.env.NODE_ENV = 'apple';
+
+        const logger = makeLogger();
+
+        expect(logger.level).toBe('debug');
+    });
 });
