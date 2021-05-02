f08ee1d918ac43edeac91d1fc11948026557e16b
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-05-01 23:43:58 -0700

    chore: add logging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 /* istanbul ignore file */
 import config from 'config';
 import server from './server';
+import logger from './logger';
 
 const port = process.env.PORT || config.get<number>('port');
 
-server.listen(port);
+server.listen(port, () => void logger.info('Server listening on localhost:' + port));
